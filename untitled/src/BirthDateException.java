0592402bd91c68cd544448dcdc20e5465d288e6d
unknown <sophanith.nit@gmail.com>
2021-05-28 17:16:31 +0700

    Ex9

---
+++ untitled/src/BirthDateException.java
@@ -0,0 +1,5 @@
+public class  BirthDateException extends Exception {
+    public BirthDateException() {
+        super("Date of birth must be a date and equals to or more than 15 years old!");
+    }
+}
